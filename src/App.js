a3719f60064b22eafb237a6adb210878f2db4d58
Triken Barman <btriken6@gmail.com>
2022-11-09 00:18:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     <>
     <Navbar/>
     <Search/> 
-    {/* <Pagination/> */}
     <Routes>
       <Route path='/' element={<MarvelGallery/>}/>
       <Route path="/details/" element={<DetailView/>}/>
